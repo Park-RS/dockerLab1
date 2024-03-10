37fec21928a7e5329ed34eccbfa65a679a204a97
Park-RS <123835717+Park-RS@users.noreply.github.com>
2024-03-10 17:29:57 +0700

    second task (docker-compose)

--- parser_1.py
+++ parser_1.py
@@ -1,11 +1,40 @@
 import requests
 from bs4 import BeautifulSoup
 
-response = requests.get('https://yandex.ru/pogoda/barnaul?lat=53.346785&lon=83.77686')
-
-soup = BeautifulSoup(response.content, 'html.parser')
-
-temp = soup.find('span', class_='temp__value temp__value_with-unit')
 
 
-print('Текущая погода в Барнауле:', temp.text.strip())
+def get_weather(city):
+    api_key = 'bd60f287e2f191f1f39ca340d4cf657f'
+    url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric'
+    
+    response = requests.get(url)
+    data = response.json()
+    
+    if data['cod'] == 200:
+        weather = {
+            'city': data['name'],
+            'temperature': data['main']['temp'],
+            'humidity': data['main']['humidity'],
+            'wind_speed': data['wind']['speed'],
+            'description': data['weather'][0]['description']
+        }
+        
+        return weather
+    else:
+        return None
+
+def main():
+    city = 'Барнаул'
+    
+    weather = get_weather(city)
+    if weather:
+        print(f'Погода в городе {weather["city"]}:')
+        print(f'Температура: {weather["temperature"]}°C')
+        print(f'Влажность: {weather["humidity"]}%')
+        print(f'Скорость ветра: {weather["wind_speed"]} м/c')
+        print(f'Описание: {weather["description"]}')
+    else:
+        print('Не удалось получить данные о погоде')
+
+if __name__ == '__main__':
+    main()
